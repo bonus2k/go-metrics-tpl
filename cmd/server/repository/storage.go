25308659586af35e5a4f95b50065f50500453d89
Katvitskii Spartak <skatvitskii@hrsinternational.com>
2023-09-02 02:16:35 +0300

    implemented iter1

---
+++ cmd/server/repository/storage.go
@@ -0,0 +1,44 @@
+package repository
+
+import "strings"
+
+type MemStorageImpl struct {
+	gauge   map[string]float64
+	counter map[string][]int64
+}
+
+func NewMemStorage() MemStorageImpl {
+	return MemStorageImpl{gauge: make(map[string]float64), counter: make(map[string][]int64)}
+}
+
+func (ms *MemStorageImpl) AddGauge(name string, value float64) {
+	trimName := strings.TrimSpace(name)
+	ms.gauge[trimName] = value
+}
+
+func (ms *MemStorageImpl) GetGauge(name string) float64 {
+	trimName := strings.TrimSpace(name)
+	return ms.gauge[trimName]
+}
+
+func (ms *MemStorageImpl) AddCounter(name string, value int64) {
+	trimName := strings.TrimSpace(name)
+	int64s, found := ms.counter[trimName]
+	if found {
+		ms.counter[trimName] = append(int64s, value)
+	} else {
+		ms.counter[trimName] = []int64{value}
+	}
+}
+
+func (ms *MemStorageImpl) GetCounter(name string) []int64 {
+	trimName := strings.TrimSpace(name)
+	return ms.counter[trimName]
+}
+
+type MemStorage interface {
+	AddGauge(string, float64)
+	GetGauge(string) float64
+	AddCounter(string, int64)
+	GetCounter(string) []int64
+}
