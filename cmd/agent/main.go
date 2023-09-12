1a3b6028399e0192432c27564dc91b230dba1e87
Katvitskii Spartak <skatvitskii@hrsinternational.com>
2023-09-12 14:01:51 +0300

    add wait group

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/bonus2k/go-metrics-tpl/cmd/agent/clients"
 	"github.com/bonus2k/go-metrics-tpl/cmd/agent/services"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -12,10 +13,10 @@ var mapMetrics map[string]string
 
 func main() {
 	parseFlags()
-	count := services.GetPollCount()
-	client := clients.Connect{Server: connectAddr, Protocol: "http"}
-	fmt.Fprintf(os.Stdout, "Connect to server %s, report interval=%d, poll interval=%d\n", connectAddr, reportInterval, pollInterval)
+	var wg sync.WaitGroup
+	wg.Add(2)
 	go func() {
+		defer wg.Done()
 		for {
 			mapMetrics = services.GetMapMetrics()
 			time.Sleep(time.Duration(pollInterval) * time.Second)
@@ -23,16 +24,20 @@ func main() {
 	}()
 
 	go func() {
+		count := services.GetPollCount()
+		client := clients.Connect{Server: connectAddr, Protocol: "http"}
+		fmt.Fprintf(os.Stdout, "Connect to server %s, report interval=%d, poll interval=%d\n", connectAddr, reportInterval, pollInterval)
+		defer wg.Done()
 		for {
 			time.Sleep(time.Duration(reportInterval) * time.Second)
 			services.AddRandomValue(mapMetrics)
 			if _, err := client.SendToGauge(mapMetrics); err != nil {
-				fmt.Fprintf(os.Stdout, "[SendToGauge] Error %v\n", err)
+				fmt.Fprintf(os.Stderr, "[SendToGauge] Error %v\n", err)
 			}
 			if _, err := client.SendToCounter("PollCount", count()); err != nil {
-				fmt.Fprintf(os.Stdout, "[SendToCounter] Error %v\n", err)
+				fmt.Fprintf(os.Stderr, "[SendToCounter] Error %v\n", err)
 			}
 		}
 	}()
-	time.Sleep(100 * time.Hour)
+	wg.Wait()
 }
