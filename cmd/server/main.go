a74349416deec3ccee967caf348aeb760cc4c7c8
Katvitskii Spartak <skatvitskii@hrsinternational.com>
2023-10-02 18:00:23 +0300

    add multierr

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 	"github.com/bonus2k/go-metrics-tpl/internal/logger"
 	"github.com/bonus2k/go-metrics-tpl/internal/server/controllers"
+	"go.uber.org/multierr"
 	"net/http"
 )
 
 func main() {
 	parseFlags()
-	err := logger.Initialize(runLog)
-	if err != nil {
-		panic(err)
-	}
+	var err error
+	err = multierr.Append(err, logger.Initialize(runLog))
 	logger.Log.Info(fmt.Sprintf("Running server on %s log level %s", runAddr, runLog))
-	err = http.ListenAndServe(runAddr, controllers.MetricsRouter())
+	err = multierr.Append(err, http.ListenAndServe(runAddr, controllers.MetricsRouter()))
 	if err != nil {
 		panic(err)
 	}
-
 }
