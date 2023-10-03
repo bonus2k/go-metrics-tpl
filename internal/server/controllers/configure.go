43ae0470291de8ff177611e104c2d0ad1e4c09da
Katvitskii Spartak <skatvitskii@hrsinternational.com>
2023-10-03 20:08:08 +0300

    add return old api

--- internal/server/controllers/configure.go
+++ internal/server/controllers/configure.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bonus2k/go-metrics-tpl/internal/logger"
 	"github.com/bonus2k/go-metrics-tpl/internal/server/repositories"
 	"github.com/go-chi/chi/v5"
+	"net/http"
 )
 
 var MemStorage repositories.MemStorage
@@ -18,7 +19,20 @@ func MetricsRouter() chi.Router {
 	router.Route("/", func(r chi.Router) {
 		r.Post("/update/", SaveMetric)
 		r.Post("/value/", GetMetric)
+		r.Post("/update/gauge/{name}/{value}", GaugePage)
+		r.Post("/update/counter/{name}/{value}", CounterPage)
+		r.Post("/update/*", func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(http.StatusBadRequest)
+		})
+		r.Post("/update/gauge/*", func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(http.StatusNotFound)
+		})
+		r.Post("/update/counter/*", func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(http.StatusNotFound)
+		})
+
 	})
 	router.Get("/", AllMetrics)
+	router.Get("/value/{type}/{name}", GetValue)
 	return router
 }
--- internal/server/controllers/handlers.go
+++ internal/server/controllers/handlers.go
@@ -2,10 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/bonus2k/go-metrics-tpl/internal/logger"
 	"github.com/bonus2k/go-metrics-tpl/internal/models"
+	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -81,7 +85,70 @@ func GetMetric(w http.ResponseWriter, r *http.Request) {
 	}
 	logger.Log.Debug("sending HTTP 200 response")
 	w.WriteHeader(http.StatusOK)
-	return
+}
+
+func CounterPage(w http.ResponseWriter, r *http.Request) {
+	if MemStorage == nil {
+		panic("storage not initialized")
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	name := chi.URLParam(r, "name")
+	value := chi.URLParam(r, "value")
+
+	if num, err := strconv.ParseInt(value, 10, 64); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else {
+		MemStorage.AddCounter(name, num)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+}
+
+func GaugePage(w http.ResponseWriter, r *http.Request) {
+	if MemStorage == nil {
+		panic("storage not initialized")
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	name := chi.URLParam(r, "name")
+	value := chi.URLParam(r, "value")
+
+	num, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	MemStorage.AddGauge(name, num)
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetValue(w http.ResponseWriter, r *http.Request) {
+	typeV := chi.URLParam(r, "type")
+	name := chi.URLParam(r, "name")
+	switch typeV {
+	case "gauge":
+		if gauge, ok := MemStorage.GetGauge(name); !ok {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			_, err := io.WriteString(w, fmt.Sprintf("%v", gauge))
+			if err != nil {
+				logger.Log.Error("[GetValue gauge]", zap.Error(err))
+			}
+		}
+	case "counter":
+		if counter, ok := MemStorage.GetCounter(name); !ok {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			_, err := io.WriteString(w, fmt.Sprintf("%v", counter))
+			if err != nil {
+				logger.Log.Error("[GetValue counter]", zap.Error(err))
+			}
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func AllMetrics(w http.ResponseWriter, r *http.Request) {
