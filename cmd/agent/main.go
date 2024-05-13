274d0cf3035804db56212b8b5fb0f2df2aab16dc
Spartak Katvitskiy <katvitsn@mts.ru>
2024-05-14 00:02:23 +0300

    add crypto

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,6 +3,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/bonus2k/go-metrics-tpl/internal/agent/clients"
+	"github.com/bonus2k/go-metrics-tpl/internal/middleware/interface/rest"
+	"github.com/go-resty/resty/v2"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -47,7 +50,35 @@ func main() {
 	chanelMetrics.GetPSUtilMetrics(pollTicker)
 	chanelResult := chanelMetrics.GetChanelResult()
 
-	pool := services.NewPool(signPass, connectAddr)
+	sha256 := rest.NewSignSHA256(signPass)
+	crypto, err := rest.NewEncrypt(cryptoKey)
+	if err != nil {
+		logger.Exit(err, 1)
+	}
+	res := resty.New().
+		SetPreRequestHook(func(client *resty.Client, request *http.Request) error {
+			err = crypto.EncryptRequest(client, request)
+			if err != nil {
+				return err
+			}
+			err = sha256.AddSignToReq(client, request)
+			if err != nil {
+				return err
+			}
+			err = rest.GzipReqCompression(client, request)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}).
+		SetRetryCount(2).
+		SetRetryWaitTime(1 * time.Second).
+		SetRetryMaxWaitTime(9 * time.Second).
+		SetCloseConnection(true)
+	client := clients.Connect{Server: connectAddr, Protocol: "http", Client: res}
+
+	pool := services.NewPool(client)
 	for i := 0; i < rateLimitRoutines; i++ {
 		num := i
 		reportTicker := time.NewTicker(time.Duration(reportInterval) * time.Second)
--- internal/server/controllers/configure.go
+++ internal/server/controllers/configure.go
@@ -10,16 +10,21 @@ import (
 )
 
 // MetricsRouter создает роутер для HTTP сервера
-func MetricsRouter(mem *repositories.Storage, pass string) chi.Router {
+func MetricsRouter(mem *repositories.Storage, pass string, file string) chi.Router {
 	ctrl := NewController(mem)
 	router := chi.NewRouter()
 	sha256 := rest.NewSignSHA256(pass)
+	decrypt, err := rest.NewDecrypt(file)
+	if err != nil {
+		logger.Exit(err, 1)
+	}
 	router.Use(
 		rest.GzipReqDecompression1,
 		rest.GzipResCompression,
 		logger.MiddlewareLog,
 		sha256.AddSignToRes,
 		sha256.CheckSignReq,
+		decrypt.DecryptRequest,
 	)
 	router.Route("/update", func(r chi.Router) {
 		r.Post("/gauge/{name}/{value}", ctrl.GaugePage)
